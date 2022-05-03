ff0849ad62586332565eaea6f125da557bc0f006
Arun Roberts <arunplaysbass@gmail.com>
2022-05-03 17:26:09 +1000

    Added gigs and a little styling.

--- public/assets/gigData.ts
+++ public/assets/gigData.ts
@@ -1,6 +1,24 @@
 import type { GigData } from '../../lib/types'
 
 const gigData: GigData = [
+    {
+        dateString: '05/05/2022',
+        date: '2022-05-05',
+        title: 'Supporting Lucy Francesca Dron w Eaglmont and Tess Hannah',
+        gigLink: 'https://www.facebook.com/events/693508358637574?acontext=%7B%22event_action_history%22%3A[%7B%22mechanism%22%3A%22your_upcoming_events_unit%22%2C%22surface%22%3A%22bookmark%22%7D]%2C%22ref_notif_type%22%3Anull%7D',
+        venue: 'Stay Gold, Brunswick, VIC',
+        venueLink: 'https://www.staygoldmelbourne.com.au/',
+        future: true
+    },
+    {
+        dateString: '09/06/2022',
+        date: '2022-06-09',
+        title: 'Having a good time in Brunswick w Wet Weather Play',
+        gigLink: 'https://www.welcometodirthand.com',
+        venue: 'Brunswick Artists Bar, Brunswick, VIC',
+        venueLink: 'https://brunswickballroom.com.au/brunswickartistsbar/',
+        future: true
+    }
     
 ]
 
